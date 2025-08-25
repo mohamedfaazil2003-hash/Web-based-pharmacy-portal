3025ba42aefd43f80e7086681cba59f02bd784a3
mohamedfaazil2003-hash <mohamedfaazil2003@gmail.com>
2025-08-25 20:37:23 +0530

    comit

---
+++ src/main/java/com/example/web_based_pharmacy_portal_system/WebBasedPharmacyPortalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_based_pharmacy_portal_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedPharmacyPortalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBasedPharmacyPortalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_based_pharmacy_portal_system/WebBasedPharmacyPortalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_based_pharmacy_portal_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedPharmacyPortalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
